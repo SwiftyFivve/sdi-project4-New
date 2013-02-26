362c7c23426f20550e1d7a89e5b5efdd67791da7
Jordan Weaver <weaver.jordan6@gmail.com>
2013-02-25 21:03:27 -0600

    first commit

---
+++ weaver-jordan-assignment4.js
@@ -0,0 +1,25 @@
+//alert("JavaScript works!");
+
+// first problem
+var numberDialed = function(phoneNumber){
+	if (outcome = phoneNumber > 100 + '-' + 000 + '-' + 0000){
+		console.log(phoneNumber + ' ' + true)
+	} else {
+		console.log(phoneNumber + ' ' + false)
+	};
+};
+
+
+//second problem
+var emailTyped = function(email){
+	if (xxx+ "@"+bbb+"."+ccc === true){
+		console.log(email + true)
+	} else {
+		console.log(email + false)
+	};
+};
+
+//main
+numberDialed('641-530-3461')
+
+emailTyped('weaver@msn.com')
