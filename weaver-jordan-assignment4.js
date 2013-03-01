08b92f99027c16c340eae4be9bb49e44832d2e06
Jordan Weaver <weaver.jordan6@gmail.com>
2013-02-28 18:02:45 -0600

    Updated

--- weaver-jordan-assignment4.js
+++ weaver-jordan-assignment4.js
@@ -1,33 +1,30 @@
 //alert("JavaScript works!");
 
-//Strings: first problem
-var numberDialed = function(phoneNumber){
-	var pattern = numberDialed.charAt(3,7)
-	if (pattern === "-"){
-		console.log(phoneNumber + " " + true)
-	} else {
-		console.log(phoneNumber + " " + false)
-	};
+
+//myLibrary
+var myLibrary = function(){
+
+	//CHECK PHONE NUMBER
+	var numberDialed = function(phoneNumber){
+				if(isNaN(val)){
+					return true
+				} else{
+					return false
+				}
+		};
+
+
+	//EMAIL FUNCTION
+
+
+	//DATE FUNCTION
+	
+
+	//RETURN OBJECT
+	return {
+		"numberDialed": numberDialed
+	}
+
 };
 
-
-//Strings: second problem
-
-var emailTyped = function(email){
-	var symbol =
-}
-
-
-//Numbers: third problem
-var dates = function(date1, date2){
-	var timeLeft = date1 - date2
-	console.log(timeLeft)
-}
-
-
-//main
-numberDialed('641-530-3461')
-
-//emailTyped('weaver@msn.com')
-
-dates([2012, 2, 18], [2013, 4, 16])
+console.log("Is this a number?" + myLibrary.numberDialed(641-530-3461))};
